8e8f6a002183e8dd3a9a9bb472d74ae2b2d941c2
Ononobi Praise <ononobip@gmail.com>
2025-01-19 00:20:30 +0100

    fix: fixed bug for record screen with chromeStreamAPI

--- camera.js
+++ camera.js
@@ -1,6 +1,5 @@
 const runCode = async () => {
     const cameraElement = document.querySelector('#camera');
-    console.log('cameraElement', cameraElement);
     // await user permission for camera
     const permission = await navigator.permissions.query({
         name: 'camera'
@@ -13,18 +12,16 @@ const runCode = async () => {
     if (permission.state === 'denied') {
         window.alert('Camera permision denied')
     }
-    console.log('permission', permission);
 
     const startCamera = async () => {
         const videoElement = document.createElement('video');
         videoElement.setAttribute('autoplay', true);
         // videoElement.setAttribute('mute', true);
-        videoElement.setAttribute('style', 'height: 200px; transform: scaleX(-1); border-radius: 50%;');
+        videoElement.setAttribute('style', 'height: 200px; transform: scaleX(-1);');
         cameraElement.appendChild(videoElement);
 
         // get stream
-
-        const cameraStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        const cameraStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
         videoElement.srcObject = cameraStream;
     }
     startCamera()
