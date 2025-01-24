a79067c923723e3607b78c4eca778773e8e3892c
Ononobi Praise <ononobip@gmail.com>
2025-01-24 19:53:14 +0100

    fix: addition of draggable feature on camera body

--- content.js
+++ content.js
@@ -10,7 +10,29 @@ if (window.camera) {
     const cameraElement = document.createElement('iframe');
     cameraElement.id = cameraId;
     cameraElement.setAttribute('allow', 'camera; microphone');
-    cameraElement.src = chrome.runtime.getURL('camera.html')
-    cameraElement.setAttribute('style', 'position: fixed; top: 50px; left: 50px; width: 200px; height: 200px; background-color: rgba(0, 0, 0, 0.5); z-index: 9999; border-radius: 50%; ');
+    cameraElement.src = chrome.runtime.getURL('camera.html');
+    cameraElement.setAttribute('style', 'position: fixed; top: 50px; left: 50px; width: 200px; height: 200px; background-color: rgba(0, 0, 0, 0.5); z-index: 9999; border-radius: 50%; cursor: grab;');
     document.body.appendChild(cameraElement);
+
+    let isDragging = false;
+    let offsetX, offsetY;
+
+    cameraElement.addEventListener('mousedown', (e) => {
+        isDragging = true;
+        offsetX = e.clientX - cameraElement.offsetLeft;
+        offsetY = e.clientY - cameraElement.offsetTop;
+        cameraElement.style.cursor = 'grabbing';
+    });
+
+    document.addEventListener('mousemove', (e) => {
+        if (isDragging) {
+            cameraElement.style.left = `${e.clientX - offsetX}px`;
+            cameraElement.style.top = `${e.clientY - offsetY}px`;
+        }
+    });
+
+    document.addEventListener('mouseup', () => {
+        isDragging = false;
+        cameraElement.style.cursor = 'grab';
+    });
 }
